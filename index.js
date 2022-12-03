be565624d5af2c0cf7902512e054b2260be7203b
OyakMasu21 <akzaki17@gmail.com>
2022-12-03 22:05:14 +0300

    Complete assignment 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Your code here
+ // Your code here
 var dodger = document.getElementById('dodger');
       
 dodger.style.backgroundColor = "#ff69b4";
@@ -28,4 +28,37 @@ document.addEventListener('keydown', function(e) {
   if ( e.which === 39 ) {
     moveDodgerRight();
   }
+}); 
+
+/*
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+//dodger.style.left = "0px";
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.right.replace("px", "");
+  const right = parseInt(rightNumbers, 10);
+
+  if(right > 0) {
+    dodger.style.right = `${right + 1}px`;
+
+  }
+}
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft"){
+  }
 });
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowRight"){
+  }
+});*/
